9df3cf1ddbecd4a4a1f730fd9a126a2317ecfe0a
Gourab <makecocgreatagain@gmail.com>
2022-07-26 14:37:11 +0530

    initial setup

--- src/Linked_List/SLL.java
+++ src/Linked_List/SLL.java
@@ -1,5 +1,43 @@
 package Linked_List;
 
 public class SLL {
+    Node head;
+    Node tail;
+    int size;
+    SLL(){
+        size=0;
 
+    }
+static class  Node {
+int value;
+Node next;
+Node(int value){
+    this.value=value;
+}
+    Node(int value,Node next){
+        this.value=value;
+        this.next= next;
+    }
+
+}
+void insertFirst(int value){
+        Node temp= new Node(value);
+        if(head==null){
+    head=temp;
+    tail=temp;
+        }
+        else{
+            temp.next=head;
+            head=temp;
+        }
+
+}
+
+void display(){
+        Node temp= head;
+        while(temp!=null){
+            System.out.println(temp.value);
+            temp= temp.next;
+        }
+}
 }
